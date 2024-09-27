04127ed9de1a3cf6f5591800e61b26b620517904
Batuhan Baysal <batuhanbaysal3@gmail.com>
2024-09-28 00:07:29 +0300

    grade-submission-field-validation

---
+++ 03-Field Validation/gradesubmission/src/main/java/com/batuhan/gradesubmission/GradesubmissionApplication.java
@@ -0,0 +1,12 @@
+package com.batuhan.gradesubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradesubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradesubmissionApplication.class, args);
+	}
+}
